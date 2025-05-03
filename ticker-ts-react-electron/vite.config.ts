b05533a6a49f26ebe2039bb46c7c52050042f26b
Dylan Mulligan <dylan.mulligan2020@gmail.com>
2025-05-03 17:15:11 -0400

    refactor: integrate Vite properly

--- ticker-ts-react-electron/vite.config.ts
+++ ticker-ts-react-electron/vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  root: '.', // Set the root directory to the public folder
+  base: './', // Use relative paths for Electron compatibility
+  publicDir: '/public', // Explicitly set the public directory
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // Alias for the src directory
+    },
+  },
   build: {
-    outDir: 'build',
+    outDir: './build', // Output directory relative to the public folder
+    emptyOutDir: true,
   },
   server: {
     port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+    strictPort: true,
   },
 });
