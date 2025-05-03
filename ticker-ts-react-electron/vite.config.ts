17c2ba6a69ab24c2ba91c8556684dcda7149abd9
Dylan Mulligan <dylan.mulligan2020@gmail.com>
2025-05-03 17:40:31 -0400

    bundle tabler icons to fix vite dev performance issue

--- ticker-ts-react-electron/vite.config.ts
+++ ticker-ts-react-electron/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'), // Alias for the src directory
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
   },
   build: {
